4403c4855b17d2b77b1e57d508d65334256b76c7
AlAminKh03 <alaminkhan6203@gmail.com>
2022-05-08 10:56:18 +0600

    Api inserted

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
 
@@ -9,7 +10,30 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/user', (req, res) => {
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.szixq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect()
+        const productCollection = client.db('sports-gear').collection('products')
+
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('server is running')
 })
 
